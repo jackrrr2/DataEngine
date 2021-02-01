a7b17420afb6bc372cbe96ae7a3a617964b27900
jackrrr2 <78346167+jackrrr2@users.noreply.github.com>
2021-02-01 17:04:54 +0800

    Add files via upload

---
+++ lesson1panda/question1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 16:58:57 2021
+
+@author: LuChaoqi
+"""
+
+
+
+sum = 0
+number = 0
+while number <101:
+    sum = sum +number
+    number = number+2
+    
+print(sum)
+
+
+    
+    
